7a9a10611df7d0c00f843ee9c3247d8414e14ebd
versatile0010 <siam0294@naver.com>
2023-09-19 17:12:02 +0900

    feat: redis publisher, subscriber

--- src/main/java/com/example/websocketchat/controller/ChatController.java
+++ src/main/java/com/example/websocketchat/controller/ChatController.java
@@ -1,6 +1,8 @@
 package com.example.websocketchat.controller;
 
 import com.example.websocketchat.dto.ChatMessage;
+import com.example.websocketchat.repository.ChatRoomRepository;
+import com.example.websocketchat.service.RedisPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -10,15 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ChatController {
     private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisPublisher publisher;
+    private final ChatRoomRepository chatRoomRepository;
 
     @MessageMapping("/chat/message") // client 에서 /pub/chat/message 으로 발행 요청 시
     public void message(ChatMessage message) {
-        if (ChatMessage.MessageType.JOIN.equals(message.getType())) { // join 일 경우
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
         }
         messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
-        // client 는 /sub/chat/room/{room-id} 으로 subscribe
-        // message 가 전달되면 화면에 출력 해주면 됨
-        // 여기서 /sub/chat/room/{room-id} 가 topic 에 해당
+        publisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
     }
 }
